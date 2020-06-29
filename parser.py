9552fd515aea3bcb26dfbe21b2d45172854264c2
Aguy11 <67076284+aguy11@users.noreply.github.com>
2020-06-29 09:59:06 -0400

    Create parser.py

---
+++ parser.py
@@ -0,0 +1,41 @@
+
+
+class Parser(object):
+
+  def __init__(self, tokens):
+    self.tokens = tokens
+    self.token_index = 0
+
+  def parse(self):
+    while self.token_index < len(self.tokens):
+      token_type = self.tokens[self.token_index][0]
+      token_value = self.tokens[self.token_index][1]
+      if token_type == "VAR_DECLARATION" and token_value == "var":
+        self.parse_variable_declaration(self.tokens[self.token_index: len(self.tokens)])
+      self.token_index += 1
+
+  def parse_variable_declaration(self, token_stream):
+    tokens_checked = 0
+    for token in range(0, len(token_stream)):
+      token_type = token_stream[tokens_checked][0]
+      token_value = token_stream[tokens_checked][1]
+      if token_type == "STATEMENT_END": 
+        break
+      if token == 0:
+        print(f'Variable type: {token_value}')
+      elif token == 1 and token_type == "IDENTIFIER":
+        print(f'Variable name: {token_value}')
+      elif token == 1 and token_type != "IDENTIFIER":
+        print(f"ERROR: Invalid variable name {token_value}")
+        quit()
+      elif token == 2 and token_type == "OPERATOR":
+        print(f'Assignment Operator: {token_value}')
+      elif token == 2 and token_type != "OPERATOR":
+        print(f'ERROR: Assignment operator missing or invalid')
+        quit()
+      elif token == 3 and token_type in ['STRING', 'INTEGER', 'IDENTIFIER']:
+          print("Variable Value " + token_value)
+      elif token == 3 and token_type not in ['STRING', 'INTEGER', 'IDENTIFIER']:
+          print("ERROR: Invalid Variable Type")
+          quit()
+      tokens_checked += 1
